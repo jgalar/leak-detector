67e5f38d38aa1961cab7e3e4762830d469c66d56
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2015-06-10 01:25:57 +0200

    Add some basic argument validation to the leak-finder stub
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- leak-detector.py
+++ leak-detector.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 import babeltrace
 import sys
+import os
+
+if len(sys.argv) != 2:
+	print("Usage: leak-detector /path/to/trace")
+	sys.exit(1)
 
 # get the trace path from the first command line argument
 trace_path = sys.argv[1]
+
+if not os.path.isdir(trace_path):
+	print("Path {} does not exist, aborting...".format(trace_path))
+	sys.exit(1)
 
 trace_collection = babeltrace.TraceCollection()
 trace_collection.add_traces_recursive(trace_path, 'ctf')
